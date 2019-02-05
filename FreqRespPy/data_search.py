92dc936e71bab1d57966d0ebe4cbf3ca6817facc
Dale Jacques <djacques@uwalumni.com>
2019-02-04 19:37:35 -0500

    .csv handling: output meta and target strengths

--- FreqRespPy/data_search.py
+++ FreqRespPy/data_search.py
@@ -19,12 +19,24 @@ def get_target_file_list(data_directory):
     return file_list
 
 
-def open_target_file(ft_region_file):
+def open_target_file(ft_region_file, meta_rows=6, transpose_meta=False):
     df = pd.read_csv(ft_region_file)
+    df = df.drop(['Variable_index', 'Variable_name'], axis=1)
 
-    meta = df
+    meta = df.head(meta_rows)
+    if transpose_meta:
+        meta = meta.transpose()
+
+    df.drop(df.head(meta_rows).index, inplace=True)
+
+    return [meta, df]
 
 
 if __name__ == '__main__':
-    data_files = get_target_files('../data/')
+    data_files = get_target_file_list('../data/')
     print(data_files)
+    file = data_files[0]
+    meta, df = open_target_file(file)
+
+    print(meta)
+    print(df)
